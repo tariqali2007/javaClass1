0235abbfbdc53e608352517749de926db4765728
tariqalimahar <tariqalimahar2007@gmail.com>
2024-05-20 07:12:54 +0500

    class1

---
+++ style.js
@@ -0,0 +1,81 @@
+// let num=77;
+
+// num =776;
+// num =33;
+// console.log(num);
+
+// let num="44";
+// let num2=66;
+
+// alert( num+ num2);
+// console.log(num + num2);
+
+// let firstname ="muhammad";
+// let lastname ="shoaaib";
+
+// console.log(firstname +"      "  + lastname);
+
+// number + number addition
+// num + String concatenartion
+// String +number concatenation
+
+
+// let email ="asd@gmail.com";
+// let name ="tariq";
+// let account =7893783267;
+// let etc
+// console.table([email , name ,account,etc])
+
+// math operators = +- * /divided % modules
+
+// let num=66;
+// let num2 =1;
+// console.log(num +num2);
+
+// let data =10;
+// let data2 =3;
+// let add =data % data2;
+// console.log(add);
+
+
+// let name=prompt("what is your name..........");
+
+// document.write(name)
+
+// let nmae=prompt("what is yuor nuber.............");
+// console.log(name);
+
+// let rendomnumbwer =Math.random();
+// rendomNumbwer=rendomNumbwer * 10;
+// let rounded =math.round(rendomnumbwer)
+// console.log(rounded)
+
+// BODMAS = BARKET OPEN  DIVIDED MULPLY ADD SUBTAR
+// let num= 4 + 6 * (5 + 5);
+// console.log(num);
+
+
+// let x =33;
+// console.log(x);
+// // console.log(typeof String(x));
+
+
+// let name = null;
+// console.log(name);
+
+// Boolean
+
+// let x =33;
+// let y =false;
+
+// console.log(typeof y);
+
+// console.log(x + y)
+
+
+// let x=33;
+// let y= true;
+// console.log(x + 12 + y);
+
+
+
